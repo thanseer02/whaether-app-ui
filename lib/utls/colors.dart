d379cd330633a21f62afd48df9ab25b7a00e1602
Muhammed Tanseer <muhammed.tanseer@mindster.com>
2025-02-09 09:27:33 +0530

    version upd colors added

--- lib/utls/colors.dart
+++ lib/utls/colors.dart
@@ -1,3 +1,8 @@
 import 'dart:ui';
+class AppColors {
+  static const Color primaryButtonColor = Color(0xFF24786D);
+  static const Color primaryColor1 = Color(0xFF262C51);
+  static const Color primaryColor2 = Color(0xFF45278B);
 
-Color get primaryButtonColor => const Color(0xFF24786D);
+  
+}
